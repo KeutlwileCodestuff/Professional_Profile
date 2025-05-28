94520514172f3e7ca48c6b75ec12ce88919a0064
WeThinkCode Student <keutlwilempolokeng@gmail.com>
2025-05-28 22:45:11 +0200

    data stored

---
+++ src/data/constants.js
@@ -0,0 +1,199 @@
+export const Bio = {
+    name: "Keutlwile Mpolokeng",
+    roles: [
+      "Frontend Developer",
+      "Backend Developer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/KeutlwileCodestuff",
+    resume:
+      "https://drive.google.com/file/d/1JyS-OHQC8UV4Gg5YQfbytHlVbeXHzLyU/view?usp=drivesdk",
+    linkedin: "https://www.linkedin.com/in/keutlwile-s-mpolokeng-708231282/",
+    twitter: "",
+    insta: "",
+    facebook: "",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Tailwind",
+          image:
+            "https://getlogovector.com/tailwind-css-logo-vector-svg/",
+        },
+
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Express Js",
+          image: "https://www.softude.com/blog/expressjs-vs-nodejs-a-developers-guide-to-choose-the-right-backend-technology"
+        },
+        {
+          name: "Postgresql",
+          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Docker",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+        },
+        {
+          name: "Netlify",
+          image:
+            "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+        },
+        {
+          name: "Figma",
+          image:
+            "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+        },
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "",
+      school: "WeThinkCode",
+      date: "Sep 2023 - Dec 2024",
+      grade: "",
+      desc: "I have Completed an NQF level in Software Development at WeThinkCode where I was introduced to development Technologies and concepts ",
+      degree: "NQF 5",
+    },
+    {
+      id: 1,
+      img: "",
+      date: "Apr 2018 - Apr 2019",
+      grade: "Acceptance to BSC",
+      desc: "I completed my class 12 high school education at St.Paul's High School.",
+      degree: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 10,
+      title: "Amazon Clone",
+      date: "Apr 2025 - Apr 2025",
+      description:
+        "This project is an Amazon e-commerce platform clone , that that some features of the original platform" ,
+      image: "https://1000logos.net/wp-content/uploads/2016/10/Amazon-Logo.png",
+      tags: [  
+        "JavaScript",
+        "CSS",
+        "HTML5"
+      ],
+      category: "web app",
+      github: "https://github.com/KeutlwileCodestuff/AmazonWebSiteClone",
+      webapp: "https://amazoneappclone.netlify.app",
+    },
+    {
+      id: 11,
+      title: "Movie App",
+      date: "Apr 2025 - Apr 2025",
+      description:
+        "A website that displays popular movies to  users using an API",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/0/0f/Sinners_2025_film_logo.png",
+      tags: [  
+        "API integration",
+        "React Js",
+        "JavaScript",
+        "HTML 5",
+        "CSS"
+      ],
+      category: "web app",
+      github: "https://github.com/KeutlwileCodestuff/MovieApp",
+      webapp: "",
+    },
+    {
+      id: 9,
+      title: "Rock Paper Scissors",
+      date: "Mar 2025 - Mar 2025",
+      description:
+        "",
+      image:
+        "https://652ac9661393ec50106bc380--bejewelled-tartufo-a9ffc3.netlify.app/static/media/andamanlogobg.45b223f18ddc0b70d93a.png",
+      tags: [  
+        "HTML 5",
+        "CSS",
+        "JavaScript"
+      ],
+      category: "web app",
+      github: "https://github.com/KeutlwileCodestuff/RockPaperScissors",
+      webapp: "",
+    },
+    {
+      id: 0,
+      title: "ToDo",
+      date: "May 2025 - May 2025",
+      description:
+        "",
+      image:
+        "",
+      tags: ["Postgresql", "Node Js", "Express Js", "JavaScript" , "Css" , "HTML 5" , ],
+      category: "web app",
+      github: "",
+      webapp: "",
+
+    },
+
+  ]
+
+  
